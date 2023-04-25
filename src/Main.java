a048b6eee78ae0ac53a7e39ec0496af3da932ac7
air <kobychev00@bk.ru>
2023-04-25 19:43:38 +0300

    ready to check

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world bl");
+
+        boolean result = ValidatorService.validate("test_123", "dfghjkiuy23", "dfghjkiuy23");
+        if (result) {
+            System.out.println("Логин и пароль корректны");
+        }
     }
 }
